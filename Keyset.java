c181bcc610431c58728773f48e8349be3ea6c561
菊地天聖 <kikuchitensei428@gmail.com>
2024-06-22 23:59:59 +0900

    コメントアウト

--- Keyset.java
+++ Keyset.java
@@ -160,7 +160,7 @@ public class Keyset extends JPanel implements KeyListener {
         }
         return directionBuilder.toString();
     }
-
+//画像パスを取得するメソッド
     private ImageIcon getDirectionIcon(String direction) {
         String imageName = switch (direction) {
             case "↖️" -> "images/up_left.png";
