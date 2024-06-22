b6817be388884ff35f5bda3d29edd2f39e850fba
菊地天聖 <kikuchitensei428@gmail.com>
2024-06-22 23:59:59 +0900

    0フレから開始はおかしいので１からにしました

--- Timers.java
+++ Timers.java
@@ -34,7 +34,7 @@ public class Timers {
             lastDirection = newcomen; // 新しい方向を更新
             flam.set(lastIndex, flam.get(lastIndex)); // 最後のカウンターを現在の値で固定するANTHROPIC_API_KEY=YOUR_API_KEY
             directions.set(lastIndex, newcomen); // 最後の方向を現在の値で固定する
-            flam.add(0); // 新しいカウンターを0で開始
+            flam.add(1); // 新しいカウンターを1で開始
             directions.add(""); // 新しい方向を追加
         } else {
             flam.set(lastIndex, flam.get(lastIndex) + 1);
@@ -42,7 +42,7 @@ public class Timers {
             // 99に到達したら新しいカウンターを追加
             if (flam.get(lastIndex) > 99) {
                 flam.set(lastIndex, 99); // カウンターを99で固定
-                flam.add(0); // 新しいカウンターを0で開始
+                flam.add(1); // 新しいカウンターを1で開始
                 directions.add(""); // 新しい方向を追加
             }
         }
