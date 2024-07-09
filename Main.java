2cbb25c75cc0d9b40d3ce6142be1fe767776e51f
菊地天聖 <kikuchitensei428@gmail.com>
2024-07-09 23:59:59 +0900

    コメント

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import javax.swing.SwingUtilities;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            Flam frame = new Flam();//ここでインスタンスかをしているようだね
+            Flam frame = new Flam();//yabbe
             frame.setVisible(true); //毎日草チャレンジ2
         });
     }
