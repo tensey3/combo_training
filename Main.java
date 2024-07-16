d9b6f13e162527ec8fd84ecd557b8fa3c899442b
菊地天聖 <kikuchitensei428@gmail.com>
2024-07-16 23:59:59 +0900

    コメント

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             Flam frame = new Flam();//ここでインスタンスかをしているよ
-            frame.setVisible(true); //ここが大事だったんだ
+            frame.setVisible(true); //ここが大事だったんだんだね
         });
     }
 }
