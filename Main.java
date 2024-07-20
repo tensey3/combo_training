7a5b0ee3ee825740ec085ef93784ebad2a6469c9
菊地天聖 <kikuchitensei428@gmail.com>
2024-07-20 10:47:32 +0900

    ゴミを消した

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ import javax.swing.SwingUtilities;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            Flam frame = new Flam();//やっべ
-            frame.setVisible(true); //毎日草チャレンジ2
+            Flam frame = new Flam();
+            frame.setVisible(true); 
         });
     }
 }
